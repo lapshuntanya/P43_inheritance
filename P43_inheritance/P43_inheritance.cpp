73ac8617899eaed6ca6c01c6a3e53a52a6eb04b5
lapshun <lapshun.tatiana@gmail.com>
2025-09-24 18:29:20 +0300

    main

--- P43_inheritance/P43_inheritance.cpp
+++ P43_inheritance/P43_inheritance.cpp
@@ -1,15 +1,21 @@
-#include "Lesson.h"
+ #include "PaidLesson.h"
 
 int main()
 {
-	Lesson a;
+	/*Lesson a;
 	Lesson b("C++", "Inheritance", "Create a base class");
 	cout << "--------------------------------\n";
 	a.showInfo();
 	cout << endl;
 	b.showInfo();
-	cout << "--------------------------------\n";
+	cout << "--------------------------------\n";*/
 
+	PaidLesson c;
+	PaidLesson d("Database", "SELECT query", "Hospital", 200);
+	cout << "--------------------------------\n";
+	c.showInfo();
+	d.showInfo();
+	cout << "--------------------------------\n";
 }
 
  
