43890eed917c1efa425736152aaff994af73c9b9
lapshun <lapshun.tatiana@gmail.com>
2025-09-24 18:22:23 +0300

    PaidLesson

--- P43_inheritance/PaidLesson.cpp
+++ P43_inheritance/PaidLesson.cpp
@@ -4,26 +4,26 @@ PaidLesson::PaidLesson()
 {
     //Lesson() -> "undefined", "undefined", "undefined"
     price = 0;
-    cout << "PaidLesson() " << this->theme << endl;
+    cout << "PaidLesson() " << theme << endl;
 }
 
 PaidLesson::PaidLesson(string theme, string info, string home, float price)
+    :Lesson(theme, info, home)
 {
+    //Lesson() -> "undefined", "undefined", "undefined"   
+    this->price = price;
+    cout << "PaidLesson(t, i, h) " << theme << endl;
 }
 
 PaidLesson::~PaidLesson()
 {
+    cout << "~PaidLesson() " << theme << endl;
 }
 
-void PaidLesson::setPrice(float price)
-{
-}
+void PaidLesson::setPrice(float price){ this->price = price; }
+float PaidLesson::getPrice() const{     return price;}
 
-float PaidLesson::getPrice() const
-{
-    return 0.0f;
-}
-
-void PaidLesson::showInfo() const
-{
+void PaidLesson::showInfo() const{
+    Lesson::showInfo(); //theme, info, home
+    cout << "Price: " << price << "UAH\n\n";
 }
